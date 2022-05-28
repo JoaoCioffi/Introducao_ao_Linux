c27246c1b9386c8f61821252b8c4dac1c2ebd9d6
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-05-28 14:48:09 -0300

    Create main.sh

---
+++ hello_world/main.sh
@@ -0,0 +1,34 @@
+# Prints something
+echo;echo "Prints Something:";echo
+echo "Hello world!" 
+echo "#----------------------------------#"
+
+
+# Current working directory
+echo;echo "Current working directory:";echo
+pwd
+echo "#----------------------------------#"
+
+
+# Lists directory content
+echo;echo "Lists directory content:";echo
+ls -l -a
+echo "#----------------------------------#"
+
+
+# F-string
+echo;echo "F-string:";echo
+X=1.225
+echo "X=${X}"
+echo "#----------------------------------#"
+
+
+# Creates a new directory
+# mkdir new_dir
+# cd new_dir
+# cd ..
+
+
+# Changing directory
+cd / #-> root
+cd $Home #-> home
